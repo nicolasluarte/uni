ee172d2a420c8e04fcf0d9d152154bd878bba74a
nicolasluarte <l.luarte@uc.cl>
2020-11-23 13:54:50 -0300

    run background and update git bash

---
+++ PHD/tracking_device/bash_scripts/update_git.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cd $HOME
+cd uni && git pull || git clone https://github.com/nicolasluarte/uni
---
+++ PHD/tracking_device/run/take_bg.py
@@ -0,0 +1,34 @@
+#!/home/nicoluarte/uni/PHD/tracking_device/environments/bg_fg/bin/python3.8
+import sys
+sys.path.append('../')
+from bg_fg import *
+import os
+import glob
+import csv
+import socket
+import argparse
+from configparser import ConfigParser
+from time import time as timer
+
+
+if __name__ == '__main__':
+    # read the arguments
+    parserArg = argparse.ArgumentParser(description='take background picture')
+    parserArg.add_argument('-d', '--file_dest', type=str, help='destination of background')
+    parserArg.add_argument('-c', '--capture', type=int, help='set the camera stream vide0 as default')
+    args = parserArg.parse_args()
+
+
+# set defaults or user specified
+# set background folder
+if args.file_dest is not None:
+    path = str(args.file_name) + '/bg.png'
+else:
+    path = '../background/bg.png'
+# set cam
+if args.capture is not None:
+    cap = int(args.capture)
+else:
+    cap = 0
+
+take_background(path, cap)
