85fdb67c84b6f5540e2a2dbc18e55224bbb8acdc
nicolasluarte <l.luarte@uc.cl>
2020-11-23 13:56:45 -0300

    comment on git update

--- PHD/tracking_device/bash_scripts/update_git.sh
+++ PHD/tracking_device/bash_scripts/update_git.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+# checks if repo exists if it does not exist it creates, else 
+# pulls the repo, check how to do this more robustly
 cd $HOME
 cd uni && git pull || git clone https://github.com/nicolasluarte/uni
