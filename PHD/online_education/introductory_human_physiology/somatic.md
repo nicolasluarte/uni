826d0f8587bae0f87a66ac541473f0ec7c0df1b5
nicolasluarte <l.luarte@uc.cl>
2020-12-11 04:02:39 -0300

    stats up to mice 3

---
+++ PHD/online_education/introductory_human_physiology/somatic.md
@@ -0,0 +1,46 @@
+# the somatic nervous system
+
+- somatic: controls skeletal muscle
+- autonomic: controls de organs
+
+- with somatic NS, neuron sends axon from the CNS to the skeletal muscle
+
+# spinal cord structure
+
+- interneurons: system that interprets the stimuli coming in 
+	- firing of the efferent neurons: sitting in the ventral horn
+- motor neuron
+	- consists of a motor neuron and all of the muscle fibers it controls
+- neuromuscular junction
+	- action potential -> open voltage gated calcium ions
+	- acetylcholine fuses with membrane and is released 
+	- post-synaptic has nicotinic AChR <- acetylcholine sodium channels
+
+# key concepts
+
+- control many types of movements
+- a motor neuron and the muscle fibers that it innervates constitute a motor unit
+- motor neurons have cell bodies located in grey matter of the ventral horn
+- the spinal cord contains interneurons; coordinate and integrate information
+
+# the control of movements
+
+- lower motor neurons
+	- muscle spindle: intrafusal fibers
+	- alpha: innervates the extrafusal muscle fibers, fibers of the main part of the muscle
+	- gamma: innvervate intrafusal fibers; parallel system to alpha, afferent neuron sensing stretch of the muscle
+	- alpha and gamma are co-activated
+- muscle sensory receptors
+	- muscle spindle <- afferent coiled to this sense stretch
+	- reciprocal innervation: contraction in agonist, relaxation in antagonistic
+		- negative feedback loop
+	- golgi tendon organ: tendon attaching muscle to the bone
+		- golgi tendon fires when the muscle is stretched
+		- max firing at maximal contraction
+		- afferent nerve ending
+		- mechanoreceptor
+	- golgi tendon reflex
+		- fixes too much tension in the muscle
+		- limits the tension
+		- protects the muscle-tendon junction
+- central pattern generators
--- PHD/stat_course/final_assignment.R
+++ PHD/stat_course/final_assignment.R
@@ -1,47 +1,101 @@
+library(caret)
+library(tidymodels)
+library(factoextra)
+library(NbClust)
+library(ggplot2)
 library(tidyverse)
-library(randomForest)
-setwd('/home/nicoluarte/uni/PHD/stat_course/')
-d <- read.csv('mice.csv', header=TRUE) %>% as_tibble()
-labs <- read.csv('mice_labels.csv', header = TRUE, sep = ',') %>% select(Behavior)
+setwd('/home/nicoluarte/uni/PHD/stat_course')
+rats <- list.files(pattern = "rat[0-9].csv") %>% 
+	map_df(~read_csv(.))
+labs <- list.files(pattern = "ratlabs[0-9].csv") %>%
+	map_df(~read.csv(.))
+dat <- bind_cols(rats, labs)
+dat$rat <- as.factor(dat$rat)
+dat <- dat %>% drop_na()
 
+# merge factor levels
+dat$Behavior <- as.factor(dat$Behavior)
+levels(dat$Behavior) <- c("0", "1", "1", "1", "0", "0", "0")
 
-data <- d %>% mutate(FRAME=str_replace(FRAME_PATH, "^0+(?!_)", "")) %>%
-  mutate(FRAME=str_replace(FRAME, "_(.*)", ""))
+# optimal clusters
+opt.k <- dat %>% group_by(rat) %>%
+	select(body_x, body_y) %>%
+	group_map(~ which.max(fviz_nbclust(.x, kmeans, method="silhouette")$data$y)) %>% unlist()
+group.n <- dat %>% group_by(rat) %>% summarise(n = n()) %>% select(n)
+aux <- map2(unlist(opt.k), unlist(group.n), ~ rep(.x, .y)) %>% unlist()
+dat <- dat %>% mutate(opt.k = aux)
 
-data <- data %>% bind_cols(labs)
-test <- randomForest(as.factor(Behavior) ~ body_x + body_y + head_x + head_y + tail_x + tail_y + ANGLES,
-    data = final,
-    ntree = 200,
-    mtry = 7, 
-    importance=TRUE)
-test
+# add features
+dat <- dat %>%
+	group_by(rat) %>%
+	mutate(lag1_x = lag(body_x, k=1)) %>%
+	mutate(lag1_y = lag(body_y, k=1))
 
-b.x <- data$body_x
-b.y <- data$body_y
-a.x <- data$head_x
-a.y <- data$head_y
-c.x <- data$tail_x
-c.y <- data$tail_y
+# k-means considering rat (groups) and optimal center size
+dat <- dat %>%
+	group_by(rat) %>%
+	nest() %>%
+	mutate(k.mdl = map(data, ~kmeans(.x %>% select(body_x, body_y), which.max(fviz_nbclust(.x %>% select(body_x, body_y), kmeans, method="silhouette")$data$y)))) %>%
+	ungroup()
+k.clust <- 1:dim(dat)[1] %>% map(., function(x) dat$k.mdl[[x]]$cluster) %>% unlist()
+dat <- dat %>%
+	unnest(c(data)) %>%
+	mutate(.cluster = k.clust)
 
-get_angle <- function(a.x, a.y, b.x, b.y, c.x, c.y){
-  A <- c(a.x, a.y)
-  B <- c(b.x, b.y)
-  C <- c(c.x, c.y)
-  a <- LearnGeom::Angle(A, B, C)
-  return(a)
-}
-angles <- pmap(list(a.x, a.y, b.x, b.y, c.x, c.y), get_angle) %>% unlist()
-final <- data %>% mutate(ANGLES = angles)
+# fit knn to each rat and report cross validated accuracy
+ctrl <- trainControl(method="repeatedcv",
+		     repeats=5,
+		     savePredictions="final",
+			sampling="down")
+dat <- dat %>%
+	group_by(rat) %>%
+	nest() %>%
+	mutate(cv.knn = map(data, function(df) train(Behavior ~ as.factor(.cluster) + lag1_x + lag1_y,
+							 data = df,
+							 method = "knn",
+							 na.action = na.omit,
+							 trControl = ctrl,
+							 tuneLength = 10))) %>%
+	mutate(cv.glm = map(data, function(df) train(Behavior ~ as.factor(.cluster) + lag1_x + lag1_y,
+							 data = df,
+							 na.action = na.omit,
+							 method = "glm",
+							 family="binomial",
+							 trControl = ctrl))) %>%
+	mutate(cv.knn.acc = map_dbl(cv.knn, function(x) max(x$results["Accuracy"]))) %>%
+	mutate(cv.glm.acc = map_dbl(cv.glm, function(x) max(x$results["Accuracy"])))
+summary(dat$cv.glm[[1]])
+dat$cv.knn
+dat
 
-p1.x <- data$body_x[1]
-p1.y <- data$body_y[1]
-p2.x <- data$head_x[1]
-p2.y <- data$head_y[1]
-p3.x <- data$tail_x[1]
-p3.y <- data$tail_y[1]
+# plot by movement by cluster
+mov.cluster <- dat %>% unnest(data)
+mov.cluster %>% 
+	ggplot(aes(x=body_x, y=body_y, color=as.factor(.cluster))) +
+	geom_path() + facet_wrap(~rat)
+mov.cluster %>% ggplot(aes(.cluster, ..count..)) + geom_bar(aes(fill=Behavior), position="dodge")
 
-sum(data$Behavior == 3)
-A <- list(c(data$head_x[1], data$head_y[1]))
-B <- list(c(data$body_x[1], data$body_y[1]))
-C <- list(c(data$tail_x[1], data$tail_y[1]))
-LearnGeom::Angle(A[[1]], B[[1]], C[[1]])
+# global model
+global.dat <- dat %>% unnest(data)
+k.folds = 3
+folds <- groupKFold(global.dat$rat, k = k.folds) 
+global.control <- trainControl(
+                        method="repeatedcv", 
+                        number=k.folds, 
+                        repeats=3,
+			preProcOptions=c("center", "scale"),
+			sampling="down",
+                        index=folds)
+global.mdl1 <- train(Behavior ~ as.factor(.cluster) + lag1_x + lag1_y,
+							 data = global.dat,
+							 method = "knn",
+							 na.action = na.omit,
+							 trControl = global.control,
+							 tuneLength = 10)
+global.mdl2 <- train(Behavior ~ as.factor(.cluster) + lag1_x + lag1_y,
+							 data = global.dat,
+							 method = "rf",
+							 metric = "Accuracy",
+							 na.action = na.omit,
+							 trControl = global.control,
+							 tuneLength = 4)
